47f3e9692622751992333cfb3fd44674b36b692d
bobo <kumakovdavid@gmail.com>
2026-08-31 22:03:07 +0800

    Fix vendor inherit path for minimal-manifest-twrp 12.1

--- twrp_yxp_713_pad.mk
+++ twrp_yxp_713_pad.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit TWRP common config.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from yxp_713_pad device
 $(call inherit-product, device/youxuepai/yxp_713_pad/device.mk)
