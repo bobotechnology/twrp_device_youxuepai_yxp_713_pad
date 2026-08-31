63fe146d0f32ea8c013a8ae34479b0ea640f1a2e
bobo <kumakovdavid@gmail.com>
2026-09-01 01:11:53 +0800

    Fix Himax touch selection and recovery ADB

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,5 +106,6 @@ PLATFORM_VERSION := 16.1.0
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_INPUT_BLACKLIST := "hbtp_vm mtk-tpd"
+TWRP_EVENT_LOGGING := true
 TW_USE_TOOLBOX := true
